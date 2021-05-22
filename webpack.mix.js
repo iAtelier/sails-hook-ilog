969fecac1af4bffc9ce900ed89754ba04a5aee79
hossein <hossein@vitualizergalaxy.xyz>
2021-05-22 16:46:22 +0200

    updating laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
-const { mix } = require('laravel-mix');
-  
+let mix = require('laravel-mix');
+
 mix
-	.js('assets/vue/index.js', 'public/assets/ilog/js/index.js')
-	.js('assets/js/vue-config', 'public/assets/ilog/js/config.js')
-	.sass('assets/sass/style.scss', 'public/assets/ilog/css/style.css');
+	.js('assets/vue/index.js', 'js')
+	.js('assets/js/config.js', 'js')
+	.sass('assets/sass/style.scss', 'css').setPublicPath('public/assets/ilog/');
